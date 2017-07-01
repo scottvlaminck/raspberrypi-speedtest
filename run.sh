a55a95e105ee99ee0f01a016d34fbbad29c0aaf8
Tom Barnes <ruralmyth@gmail.com>
2017-07-02 00:00:15 +0100

    better handling of network errors

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+set -o pipefail
 
 PATH=$PATH:/usr/local/bin
 
 cd `dirname $0`
-../speedtest-cli-extras/bin/speedtest-csv | python gsheet_add.py
+
+curl "http://www.google.co.uk" --max-time 5
+result=$?
+
+if [ $result -ne 0 ]; then
+  echo "error" | python gsheet_add.py
+else
+  ../speedtest-cli-extras/bin/speedtest-csv | python gsheet_add.py
+fi
